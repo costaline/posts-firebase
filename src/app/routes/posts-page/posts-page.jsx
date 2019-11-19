a8bf244cf08cd12cc9fa4ffad70c0d8255aea585
Konstantin Zubarev <kv.zubarev@gmail.com>
2019-11-19 18:40:33 +0600

    connect to firebase db

--- src/app/routes/posts-page/posts-page.jsx
+++ src/app/routes/posts-page/posts-page.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import { firebase } from "~services/axios";
 
 const PostsPage = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await firebase.get("/posts.json");
+      console.log(response);
+    };
+    fetchData();
+  }, []);
+
   return (
     <section>
       <h2>PostsPage works</h2>
