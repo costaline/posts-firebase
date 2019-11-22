4a85ad04c1a5ad493bb3e7d5fb3b9a1e9b4b1ea6
Konstantin Zubarev <kv.zubarev@gmail.com>
2019-11-22 14:40:47 +0600

    update post-item

--- src/app/components/post-item/post-item.jsx
+++ src/app/components/post-item/post-item.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { format } from "date-fns";
 
-const PostItem = ({ post: { title, body } }) => {
+const PostItem = ({ post: { title, body, date } }) => {
+  const postDate = format(new Date(date), "yyyy-MM-dd");
+
   return (
     <div>
       <h4>{title}</h4>
       <p>{body}</p>
+      <small>{postDate}</small>
     </div>
   );
 };
@@ -13,7 +17,8 @@ const PostItem = ({ post: { title, body } }) => {
 PostItem.propTypes = {
   post: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    body: PropTypes.string.isRequired
+    body: PropTypes.string.isRequired,
+    date: PropTypes.number
   })
 };
 
