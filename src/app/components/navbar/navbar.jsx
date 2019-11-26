18e1d41e7829a7efca2b930043a1f3baaadb2c05
Konstantin Zubarev <kv.zubarev@gmail.com>
2019-11-26 12:34:49 +0600

    update navbar

--- src/app/components/navbar/navbar.jsx
+++ src/app/components/navbar/navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
+import PropTypes from "prop-types";
 
 import { authActions } from "~store/actions";
 
@@ -22,26 +23,28 @@ const NavBar = ({ user, logoutUser }) => {
               <NavLink to="/new-post">NEW POST</NavLink>
             </li>
           )}
+        </ul>
 
-          {!user && (
+        <ul>
+          {user ? (
+            <>
+              <li>{user.email}</li>
+              <li onClick={logoutUser}>LOGOUT</li>
+            </>
+          ) : (
             <li>
               <NavLink to="/auth">AUTH</NavLink>
             </li>
           )}
         </ul>
-
-        {user && (
-          <ul>
-            <li>
-              <NavLink to="/new-post">NEW POST</NavLink>
-            </li>
-            <li>{user.email}</li>
-            <li onClick={logoutUser}>LOGOUT</li>
-          </ul>
-        )}
       </nav>
     </header>
   );
+};
+
+NavBar.propTypes = {
+  user: PropTypes.shape({ email: PropTypes.string }),
+  logoutUser: PropTypes.func.isRequired
 };
 
 const mapStateToProps = (state) => {
