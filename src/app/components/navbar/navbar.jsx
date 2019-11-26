32d61ab3dc4ac19612fb2b9a57846e3c2b8a9d8f
Konstantin Zubarev <kv.zubarev@gmail.com>
2019-11-26 11:18:54 +0600

    logout in navbar

--- src/app/components/navbar/navbar.jsx
+++ src/app/components/navbar/navbar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
-const NavBar = () => {
+import { authActions } from "~store/actions";
+
+const NavBar = ({ user, logoutUser }) => {
   return (
     <header>
       <nav>
@@ -12,16 +16,42 @@ const NavBar = () => {
           <li>
             <NavLink to="/posts">POSTS</NavLink>
           </li>
-          <li>
-            <NavLink to="/new-post">NEW POST</NavLink>
-          </li>
-          <li>
-            <NavLink to="/auth">AUTH</NavLink>
-          </li>
+
+          {user && (
+            <li>
+              <NavLink to="/new-post">NEW POST</NavLink>
+            </li>
+          )}
+
+          {!user && (
+            <li>
+              <NavLink to="/auth">AUTH</NavLink>
+            </li>
+          )}
         </ul>
+
+        {user && (
+          <ul>
+            <li>
+              <NavLink to="/new-post">NEW POST</NavLink>
+            </li>
+            <li>{user.email}</li>
+            <li onClick={logoutUser}>LOGOUT</li>
+          </ul>
+        )}
       </nav>
     </header>
   );
 };
 
-export default NavBar;
+const mapStateToProps = (state) => {
+  return {
+    user: state.authReducer.user
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ logoutUser: authActions.logoutUser }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
