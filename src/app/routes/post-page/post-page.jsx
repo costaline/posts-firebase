d8549f84fbbc7587b6c5a369a530f4b3da4b7a86
Konstantin Zubarev <kv.zubarev@gmail.com>
2019-11-27 12:00:33 +0600

    add delete actions to post page

--- src/app/routes/post-page/post-page.jsx
+++ src/app/routes/post-page/post-page.jsx
@@ -14,7 +14,8 @@ class PostPage extends Component {
       title: PropTypes.string,
       body: PropTypes.string,
       date: PropTypes.number,
-      email: PropTypes.string
+      email: PropTypes.string,
+      user_id: PropTypes.string
     }),
     requesting: PropTypes.bool.isRequired,
     error: PropTypes.object,
@@ -22,6 +23,9 @@ class PostPage extends Component {
       params: PropTypes.shape({
         id: PropTypes.string.isRequired
       })
+    }),
+    user: PropTypes.shape({
+      id: PropTypes.string
     })
   };
 
@@ -29,8 +33,28 @@ class PostPage extends Component {
     this.props.fetchPost(this.props.match.params.id);
   }
 
+  deletePost = () => {
+    const {
+      match: {
+        params: { id }
+      },
+      history
+    } = this.props;
+
+    this.props.deletePost(id, history);
+  };
+
   render() {
-    const { title, body, date, email = "unknown" } = this.props.post;
+    const {
+      title,
+      body,
+      date,
+      email = "unknown",
+      user_id: post_user_id
+    } = this.props.post;
+
+    const { id: current_user_id } = this.props.user;
+
     const { requesting, error } = this.props;
 
     const postDate = date ? format(new Date(date), "yyyy-MM-dd") : "unknown";
@@ -41,10 +65,18 @@ class PostPage extends Component {
           <Loader />
         ) : (
           <div>
-            <h2>{title}</h2>
-            <p>{body}</p>
-            <small>{postDate}</small>
-            <cite>{email}</cite>
+            <div>
+              <h2>{title}</h2>
+              <p>{body}</p>
+              <small>{postDate}</small>
+              <cite>{email}</cite>
+            </div>
+            {post_user_id === current_user_id && (
+              <div>
+                <button onClick={this.deletePost}>DELETE</button>
+                <button>EDIT</button>
+              </div>
+            )}
           </div>
         )}
         {error ? <p>Something wrong...</p> : null}
@@ -57,12 +89,16 @@ const mapStateToProps = (state) => {
   return {
     post: state.postReducer.post,
     requesting: state.postReducer.requesting,
-    error: state.postReducer.error
+    error: state.postReducer.error,
+    user: state.authReducer.user
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({ fetchPost: postActions.fetchPost }, dispatch);
+  return bindActionCreators(
+    { fetchPost: postActions.fetchPost, deletePost: postActions.deletePost },
+    dispatch
+  );
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(PostPage);
