57f214a04dd3aaca68813f92e214e7274b1f2e42
Konstantin Zubarev <kv.zubarev@gmail.com>
2019-11-28 17:48:13 +0600

    auth-page: validate form

--- src/app/routes/auth-page/auth-page.jsx
+++ src/app/routes/auth-page/auth-page.jsx
@@ -3,11 +3,29 @@ import { connect } from "react-redux";
 
 import { authActions } from "~store/actions";
 
+const initialState = () => ({
+  email: {
+    value: "",
+    isValid: false,
+    showError: false,
+    errors: {
+      empty: true,
+      email: true
+    }
+  },
+  password: {
+    value: "",
+    isValid: false,
+    showError: false,
+    errors: {
+      empty: true,
+      minLength: true
+    }
+  }
+});
+
 class AuthPage extends Component {
-  state = {
-    email: "",
-    password: ""
-  };
+  state = initialState();
 
   componentDidMount() {
     this.replacePage();
@@ -24,67 +42,178 @@ class AuthPage extends Component {
   };
 
   onChangeHandler = (evt) => {
-    this.setState({ [evt.target.name]: evt.target.value });
+    const controlName = evt.target.name;
+    const controlValues = { ...this.state[evt.target.name] };
+    controlValues.value = evt.target.value;
+
+    const values = this.validateControl(controlName, controlValues);
+
+    values.showError = false;
+
+    this.setState({
+      [evt.target.name]: values
+    });
+  };
+
+  //TODO split
+  validateControl = (controlName, controlValues) => {
+    // eslint-disable-next-line no-case-declarations, no-useless-escape
+    const REGEX_EMAIL = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    const PASS_MIN_LENGTH = 6;
+
+    let isValid = true;
+
+    const { errors, value } = controlValues;
+
+    errors.empty = value.trim() === "" ? true : false;
+
+    isValid = !errors.empty && isValid;
+
+    switch (controlName) {
+      case "email":
+        errors.email = !REGEX_EMAIL.test(value) ? true : false;
+
+        isValid = !errors.email && isValid;
+        break;
+
+      case "password":
+        errors.minLength = value.length < PASS_MIN_LENGTH ? true : false;
+
+        isValid = !errors.minLength && isValid;
+        break;
+    }
+
+    controlValues.isValid = isValid;
+
+    return controlValues;
+  };
+
+  showFormError = () => {
+    Object.keys(this.state).forEach((control) => {
+      let controlValues = { ...this.state[control] };
+
+      if (!this.state[control].isValid) {
+        controlValues.showError = true;
+        this.setState({ [control]: controlValues });
+      }
+    });
+  };
+
+  validateForm = () => {
+    let isFormValid = true;
+
+    Object.keys(this.state).forEach(
+      (control) => (isFormValid = this.state[control].isValid && isFormValid)
+    );
+
+    if (!isFormValid) {
+      this.showFormError();
+    }
+
+    return isFormValid;
   };
 
   onSubmitHandler = (evt) => {
     evt.preventDefault();
 
-    this.setState({ email: "", password: "" });
+    this.setState(initialState);
   };
 
+  //TODO optimize
   authSignUp = () => {
     const authData = {
-      email: this.state.email,
-      password: this.state.password,
+      email: this.state.email.value,
+      password: this.state.password.value,
       returnSequreToken: true
     };
 
     this.props.firebaseSignUp(authData);
 
-    this.setState({ email: "", password: "" });
+    this.setState(initialState);
   };
 
   authSignIn = () => {
     const authData = {
-      email: this.state.email,
-      password: this.state.password,
+      email: this.state.email.value,
+      password: this.state.password.value,
       returnSequreToken: true
     };
 
     this.props.firebaseSignIn(authData);
 
-    this.setState({ email: "", password: "" });
+    this.setState(initialState);
+  };
+
+  onLoginClickHandler = () => {
+    const isFormValid = this.validateForm();
+
+    if (isFormValid) {
+      this.authSignIn();
+    }
+  };
+
+  onRegClickHandler = () => {
+    const isFormValid = this.validateForm();
+
+    if (isFormValid) {
+      this.authSignUp();
+    }
   };
 
   render() {
+    const { email, password } = this.state;
+
     return (
       <div>
         <form onSubmit={this.onSubmitHandler}>
-          <label htmlFor="auth-email">Email</label>
-          <input
-            value={this.state.email}
-            onChange={this.onChangeHandler}
-            type="email"
-            name="email"
-            id="auth-email"
-          />
+          <div>
+            <label htmlFor="auth-email">Email</label>
+            <input
+              value={email.value}
+              onChange={this.onChangeHandler}
+              type="email"
+              name="email"
+              id="auth-email"
+            />
+            {email.showError && !email.isValid && (
+              <div>
+                <small>
+                  {(email.errors.empty && "empty value") ||
+                    (email.errors.email && "invalid email")}
+                </small>
+              </div>
+            )}
+          </div>
 
-          <label htmlFor="auth-password">Password</label>
-          <input
-            value={this.state.password}
-            onChange={this.onChangeHandler}
-            type="password"
-            name="password"
-            id="auth-password"
-          />
+          <div>
+            <label htmlFor="auth-password">Password</label>
+            <input
+              value={password.value}
+              onChange={this.onChangeHandler}
+              type="password"
+              name="password"
+              id="auth-password"
+            />
+            {password.showError && !password.isValid && (
+              <div>
+                <small>
+                  {(password.errors.empty && "empty value") ||
+                    (password.errors.minLength &&
+                      "at least 6 character required")}
+                </small>
+              </div>
+            )}
+          </div>
 
-          <button onClick={this.authSignIn} type="button">
-            LOGIN
-          </button>
-          <button onClick={this.authSignUp} type="button">
-            REG
-          </button>
+          <div>
+            <button onClick={this.onLoginClickHandler} type="button">
+              LOGIN
+            </button>
+            <button onClick={this.onRegClickHandler} type="button">
+              REG
+            </button>
+          </div>
         </form>
       </div>
     );
