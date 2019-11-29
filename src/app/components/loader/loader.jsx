ee4db07d6579ca5509c8bfa0c059e550531b2425
Konstantin Zubarev <kv.zubarev@gmail.com>
2019-11-29 16:04:44 +0600

    loader as styled-component

--- src/app/components/loader/loader.jsx
+++ src/app/components/loader/loader.jsx
@@ -1,15 +1,63 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
 
-import styles from "./loader.module.scss";
+const ldsFasebook = keyframes`
+  0% {
+    top: 8px;
+    height: 64px;
+  }
+  50%,
+  100% {
+    top: 24px;
+    height: 32px;
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledLoader = styled.div`
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+
+  & div {
+    display: inline-block;
+    position: absolute;
+    left: 8px;
+    width: 16px;
+    background: ${({ color }) => color};
+    animation: ${ldsFasebook} 1.2s cubic-bezier(0, 0.5, 0.5, 1) infinite;
+
+    &:nth-child(1) {
+      left: 8px;
+      animation-delay: -0.24s;
+    }
+
+    &:nth-child(2) {
+      left: 32px;
+      animation-delay: -0.12s;
+    }
+
+    &:nth-child(3) {
+      left: 56px;
+      animation-delay: 0;
+    }
+  }
+`;
 
 const Loader = () => (
-  <div className={styles.center}>
-    <div className={styles["lds-facebook"]}>
+  <Wrapper>
+    <StyledLoader color={"grey"}>
       <div />
       <div />
       <div />
-    </div>
-  </div>
+    </StyledLoader>
+  </Wrapper>
 );
 
 export default Loader;
